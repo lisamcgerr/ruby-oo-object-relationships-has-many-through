d4018e82adc3fbb2e2d5190cce2f9e85c9cf0fa2
Lisa McGerr <lisamarie.mcgerr@gmail.com>
2020-12-13 23:33:20 -0600

    Done.

--- lib/meal.rb
+++ lib/meal.rb
@@ -1,3 +1,23 @@
+require 'pry'
+
 class Meal
 
+    attr_accessor :waiter, :customer, :total, :tip, :customer, :waiter
+    
+    @@all = [ ]
+    def initialize(waiter, customer, total, tip = 0)
+        @waiter = waiter
+        @customer = customer
+        @total = total
+        @tip = tip
+
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
 end
+
+big_mac = Meal.new("sam", "lisa", 12, 3)
+#binding.pry
